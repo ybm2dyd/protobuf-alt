cb3c4a6356587e7d69148a6a7e2b6c2507bd4e5f
gengbing <edwingeng@126.com>
2019-10-12 14:40:52 +0800

    GOGOPROTO_VIZ_LEVEL now carries the default vizLevel

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -3,6 +3,7 @@ package watcher
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
@@ -10,7 +11,10 @@ import (
 
 var (
 	fieldEnvelope = false
-	vizLevel      = false
+	vizLevel      = struct {
+		enabled bool
+		level   int
+	}{}
 )
 
 func init() {
@@ -19,8 +23,13 @@ func init() {
 		fieldEnvelope = true
 	}
 	level, ok := os.LookupEnv("GOGOPROTO_VIZ_LEVEL")
-	if ok && strings.TrimSpace(level) == "1" {
-		vizLevel = true
+	if ok {
+		n, err := strconv.Atoi(level)
+		if err != nil {
+			panic("invalid GOGOPROTO_DEF_VIZ_LEVEL")
+		}
+		vizLevel.enabled = true
+		vizLevel.level = n
 	}
 }
 
@@ -38,7 +47,7 @@ func PrintFieldEnvelope(g *generator.Generator, messageName, fieldName string) f
 }
 
 func vizLevelFuncParam() string {
-	if !vizLevel {
+	if !vizLevel.enabled {
 		return ""
 	} else {
 		return "vizLevel int8"
@@ -46,7 +55,7 @@ func vizLevelFuncParam() string {
 }
 
 func vizLevelFuncArgument() string {
-	if !vizLevel {
+	if !vizLevel.enabled {
 		return ""
 	} else {
 		return "vizLevel"
@@ -54,12 +63,12 @@ func vizLevelFuncArgument() string {
 }
 
 func PrintFuncSignatureOfSize(g *generator.Generator, ccTypeName, sizeName string) {
-	if !vizLevel {
+	if !vizLevel.enabled {
 		g.P(`func (m *`, ccTypeName, `) `, sizeName, `() (n int) {`)
 	} else {
 		g.P(`func (m *`, ccTypeName, `) `, sizeName, `() (n int) {`)
 		g.In()
-		g.P(`return m.`, sizeName, `WithVizLevel(0)`)
+		g.P(`return m.`, sizeName, `WithVizLevel(`, vizLevel.level, `)`)
 		g.Out()
 		g.P("}")
 		g.P(`func (m *`, ccTypeName, `) `, sizeName, `WithVizLevel(`, vizLevelFuncParam(), `) (n int) {`)
@@ -67,7 +76,7 @@ func PrintFuncSignatureOfSize(g *generator.Generator, ccTypeName, sizeName strin
 }
 
 func FuncInvocationOfSize(sizeName string) string {
-	if !vizLevel {
+	if !vizLevel.enabled {
 		return sizeName + "()"
 	} else {
 		return fmt.Sprintf("%sWithVizLevel(%s)", sizeName, vizLevelFuncArgument())
@@ -75,12 +84,12 @@ func FuncInvocationOfSize(sizeName string) string {
 }
 
 func PrintFuncSignatureOfMarshal(g *generator.Generator, ccTypeName string) {
-	if !vizLevel {
+	if !vizLevel.enabled {
 		g.P(`func (m *`, ccTypeName, `) Marshal() (dAtA []byte, err error) {`)
 	} else {
 		g.P(`func (m *`, ccTypeName, `) Marshal() (dAtA []byte, err error) {`)
 		g.In()
-		g.P(`return m.MarshalWithVizLevel(0)`)
+		g.P(`return m.MarshalWithVizLevel(`, vizLevel.level, `)`)
 		g.Out()
 		g.P("}")
 		g.P(`func (m *`, ccTypeName, `) MarshalWithVizLevel(`, vizLevelFuncParam(), `) (dAtA []byte, err error) {`)
@@ -88,12 +97,12 @@ func PrintFuncSignatureOfMarshal(g *generator.Generator, ccTypeName string) {
 }
 
 func PrintFuncSignatureOfMarshalTo(g *generator.Generator, ccTypeName, methodName string) {
-	if !vizLevel {
+	if !vizLevel.enabled {
 		g.P(`func (m *`, ccTypeName, `) `, methodName, `(dAtA []byte) (int, error) {`)
 	} else {
 		g.P(`func (m *`, ccTypeName, `) `, methodName, `(dAtA []byte) (int, error) {`)
 		g.In()
-		g.P(`return m.`, methodName, `WithVizLevel(dAtA, 0)`)
+		g.P(`return m.`, methodName, `WithVizLevel(dAtA, `, vizLevel.level, `)`)
 		g.Out()
 		g.P("}")
 		g.P(`func (m *`, ccTypeName, `) `, methodName, `WithVizLevel(dAtA []byte, `, vizLevelFuncParam(), `) (int, error) {`)
@@ -101,7 +110,7 @@ func PrintFuncSignatureOfMarshalTo(g *generator.Generator, ccTypeName, methodNam
 }
 
 func FuncInvocationOfMarshalTo(methodName, dAtA string) string {
-	if !vizLevel {
+	if !vizLevel.enabled {
 		return fmt.Sprintf("%s(%s)", methodName, dAtA)
 	} else {
 		return fmt.Sprintf("%sWithVizLevel(%s, %s)", methodName, dAtA, vizLevelFuncArgument())
